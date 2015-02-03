fcc58bb0d1664998fb17572b767b6290aee174a1
Julien Sagot <ju.sagot@gmail.com>
2015-02-03 10:37:49 +0100

    Added GPL license (as Bisect requires it).

--- src/ocverallsCI.ml
+++ src/ocverallsCI.ml
@@ -1,3 +1,23 @@
+(*
+    Ocveralls - Generate JSON file for http://coveralls.io API
+                from Bisect (http://bisect.x9c.fr) data.
+
+    Copyright (C) 2015 Julien Sagot
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 (* Out-of-the-box supported CI list.
  * Fisrt element of a pair is the environement variable to test
  * Second element is the pair composed of the "service_name"
